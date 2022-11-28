735c61a94d641b75c4f46032698c97a4128f9b1f
Ethan Louis <ethanlouis768@gmail.com>
2022-11-27 18:00:57 -0600

    Database login and tied userID Attached new DATABASE

--- LoginForm.cs
+++ LoginForm.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +18,7 @@ namespace EmployeeManagementSystem
         public LoginForm()
         {
             InitializeComponent();
+            
         }
 
         private void showPasswordCheckBox_CheckedChanged(object sender, EventArgs e)
@@ -34,9 +38,63 @@ namespace EmployeeManagementSystem
 
         private void loginButton_Click(object sender, EventArgs e)
         {
+
+
+            
+
+            
+            
+            SqlConnection cnn = ApplicationManager.ConnectToDatabase();
+
+            if(cnn == null) { return; }
+
+
+            string checkPassword;
+            checkPassword = "SELECT password, userID FROM ManagerLogin WHERE username='" + userNameTextBox.Text + "';";
+            
+            SqlCommand sqlCommand = new SqlCommand(checkPassword, cnn);
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+
+            // Setting the data in reader;
+            string userPassword = "";
+            string userID = "";
+            while (reader.Read())
+            {
+                userPassword = String.Format("{0}", reader[0]);
+                userID = String.Format("{0}", reader[1]);
+            }
+
+
+            cnn.Close();
+
+            // Check if password in database is = to password typed;
+            if(userPassword == passwordMaskedTextBox.Text)
+            {
+                
+                ShowSelectionScreen(userID);
+
+            }
+
+
+
+
+
+
+        }
+
+        
+
+        private void ShowSelectionScreen(string userID)
+        {
+
             TransitionForm TransitionForm = new TransitionForm();
             TransitionForm.Show();
+            TransitionForm.UpdateGreetingLabel(userID);
             this.Hide();
+
         }
     }
 }
+
